6225dda30f0753f1ec4d18035878b6167fb24dd8
telgindy <tarek.elgindy@nrel.gov>
2020-03-31 03:22:20 -0600

    restructring outputs

--- convert.py
+++ convert.py
@@ -1,11 +1,10 @@
+import sys
+import datetime
 import json
 import math
 import os
 import pandas as pd
 import opendssdirect as dss
-from ditto.store import Store
-from ditto.writers.opendss.write import Writer
-from ditto.readers.urbanopt_ditto_reader.read import Reader
 
 def get_all_voltages():
     """Computes over and under voltages for all buses"""
@@ -24,7 +23,7 @@ def get_all_voltages():
 
     return voltage_dict
 
-def get_line_overloads():
+def get_line_loading():
     """Computes the loading for Lines."""
     
     line_overloads_dict = {}
@@ -44,8 +43,7 @@ def get_line_overloads():
             
         # The loading is the ratio of the two
         ldg = max(line_current)/float(line_limit)
-        if ldg>1:
-            line_overloads_dict[line_name] = max(line_current)/float(line_limit)
+        line_overloads_dict[line_name] = max(line_current)/float(line_limit)
             
         # Move on to the next line
         flag = dss.ActiveClass.Next()
@@ -59,7 +57,6 @@ def get_xfmr_overloads(ub=1.0):
     #####################################
     #
     transformer_violation_dict ={}
-    unloaded_transformers_dict = {}
     dss.Circuit.SetActiveClass("Transformer")
     flag = dss.ActiveClass.First()
     while flag > 0:
@@ -89,101 +86,104 @@ def get_xfmr_overloads(ub=1.0):
 
         # Compute the loading
         ldg = max(transformer_current)/transformer_limit_per_phase
-        
-        # If the loading is more than 100%, store the violation
-        if ldg > ub:
-            transformer_violation_dict[transformer_name] = {'Bus': primary_bus, 'Loading (p.u.)': ldg, 'kVA' : kva, 'number_of_phases': n_phases}
-        elif ldg == 0:
-            unloaded_transformers_dict[transformer_name] = {'Bus': primary_bus, 'kVA' : kva, 'number_of_phases': n_phases}
+        transformer_violation_dict[transformer_name] = ldg
         
         # Move on to the next Transformer...
         flag = dss.ActiveClass.Next()
     
       
-    return transformer_violation_dict, unloaded_transformers_dict
+    return transformer_violation_dict
 
+config_file = sys.argv[1]
+data = open(config_file)
+config_data = json.load(data)
+geojson_file = os.path.abspath(config_data['geojson_file'])
+urbanopt_scenario = os.path.abspath(config_data['urbanopt_scenario'])
+equipment_file = os.path.abspath(config_data['equipment_file'])
+dss_analysis = os.path.abspath(config_data['opendss_folder'])
+ditto_folder = os.path.abspath(config_data['ditto_folder'])
+use_reopt = config_data['use_reopt']
+data.close()
 
+sys.path.insert(0,ditto_folder) # don't append in case there are other multiple DiTTo installations.
 
-def get_voltage_violations():
-    """Computes over and under voltages for all buses"""
-    overvoltages_dict = {}
-    undervoltages_dict = {}
-    #vmag_pu = dss.Circuit.AllBusMagPu()
-    #print(len(vmag_pu))
-    bus_names = dss.Circuit.AllBusNames()
-    dss.PVsystems.Name('pv_load_p1ulv7544')
-    for b in bus_names:
-        dss.Circuit.SetActiveBus(b)
-        vang = dss.Bus.puVmagAngle()
-        maxv = max(vang[::2])
-        minv = min(vang[::2])
-        if maxv>1.05:
-            overvoltages_dict[b] = maxv
-        if minv<0.95:
-            undervoltages_dict[b] = minv
-    
-    return undervoltages_dict, overvoltages_dict
+from ditto.store import Store
+from ditto.writers.opendss.write import Writer
+from reader.read import Reader
 
-
-
-
-base_folder = os.path.join('C:\\','Users','telgindy','Documents','Urbanopt')
-geojson_file = os.path.join(base_folder,'urbanopt-example-geojson-project','example_electrical_with_sub_one_junction.json')
-geojson_file = os.path.join(base_folder,'urbanopt-example-geojson-project','example_final_pretty.json')
-equipment_file = os.path.join(base_folder,'urbanopt-example-geojson-project','electrical_database.json')
-load_folder = os.path.join('C:\\','Users','telgindy','Documents','Urbanopt','baseline_scenario','baseline_scenario')
-dss_analysis = os.path.join('C:\\','Users','telgindy','Documents','Urbanopt','baseline_scenario','opendss')
-feature_file = os.path.join('C:\\','Users','telgindy','Documents','Urbanopt','baseline_scenario_old','baseline_scenario','baseline_features.json')
-
-timeseries_location = 'profiles'
+timeseries_location = os.path.join(dss_analysis,'profiles')
 model = Store()
-reader = Reader(geojson_file=geojson_file,equipment_file = equipment_file,load_folder = load_folder,feature_file = feature_file,is_timeseries = True, timeseries_location = timeseries_location, relative_timeseries_location = os.path.join('..','profiles'))
+reader = Reader(geojson_file=geojson_file,equipment_file = equipment_file,load_folder = urbanopt_scenario,use_reopt = use_reopt,is_timeseries = True, timeseries_location = timeseries_location, relative_timeseries_location = os.path.join('..','profiles'))
 reader.parse(model)
 
-#from ditto.modify.system_structure import system_structure_modifier
-#model.set_names()
-#modifier = system_structure_modifier(model,"source")
-#modifier.set_nominal_voltages()
+if not os.path.exists(os.path.join(dss_analysis,'dss_files')):
+    os.makedirs(os.path.join(dss_analysis,'dss_files'),exist_ok=True)
+if not os.path.exists(os.path.join(dss_analysis,'results','Features')):
+    os.makedirs(os.path.join(dss_analysis,'results','Features'),exist_ok=True)
+if not os.path.exists(os.path.join(dss_analysis,'results','Transformers')):
+    os.makedirs(os.path.join(dss_analysis,'results','Transformers'),exist_ok=True)
+if not os.path.exists(os.path.join(dss_analysis,'results','Lines')):
+    os.makedirs(os.path.join(dss_analysis,'results','Lines'),exist_ok=True)
 
-writer = Writer(output_path = 'output',split_feeders=False,split_substations=False)
+writer = Writer(output_path = os.path.join(dss_analysis,'dss_files'),split_feeders=False,split_substations=False)
 writer.write(model)
 
 ts = pd.read_csv(os.path.join(timeseries_location,'timestamps.csv'),header=0)
 number_iterations = len(ts)
-voltages_dict = {}
-overvoltages_dict = {}
-undervoltages_dict = {}
-line_overloads_dict = {}
-transformer_overloads_dict = {}
+
+voltage_df_dic = {}
+line_df_dic = {}
+transformer_df_dic = {}
+
+delta = datetime.datetime.strptime(ts.loc[1]['Datetime'],'%Y/%m/%d %H:%M:%S') - datetime.datetime.strptime(ts.loc[0]['Datetime'],'%Y/%m/%d %H:%M:%S')
+interval = delta.seconds/3600.0
+stepsize = 60*interval
+building_map = {}
+
+geojson_content = []
+try:
+    with open(geojson_file,'r') as f:
+        geojson_content = json.load(f)
+except:
+    raise IOError("Problem trying to read json from file "+ geojson_file)
+
+for element in geojson_content["features"]:
+    if 'properties' in element and 'type' in element['properties'] and 'buildingId' in element['properties'] and element['properties']['type'] == 'ElectricalJunction':
+        building_map[element['properties']['id']] = element['properties']['buildingId']
+
+
+
 for i, row in ts.iterrows():
-    #if i>10:
-    #    break
     time = row['Datetime']
     print(i,flush=True)
-    hour = int(i/4)
-    seconds = (i%4)*3600
+    hour = int(i/(1/interval))
+    seconds = (i%(1/interval))*3600
     dss.run_command("Clear")
     dss.run_command("Redirect output/Master.dss")
-    dss.run_command("Solve mode=yearly stepsize=15m number=1 hour="+str(hour)+" sec="+str(seconds))
+    dss.run_command("Solve mode=yearly stepsize="+str(stepsize)+"m number=1 hour="+str(hour)+" sec="+str(seconds))
     voltages = get_all_voltages()
-    line_overloads = get_line_overloads()
-    undervoltages,overvoltages = get_voltage_violations()
-    overloaded_xfmrs, unloaded_xfmrs = get_xfmr_overloads()
+    line_overloads = get_line_loading()
+    overloaded_xfmrs = get_xfmr_overloads()
 
-    voltages_dict[time] = voltages
-    overvoltages_dict[time] = overvoltages
-    undervoltages_dict[time] = undervoltages
-    line_overloads_dict[time] = line_overloads
-    transformer_overloads_dict[time] = overloaded_xfmrs
-if not os.path.exists(dss_analysis):
-    os.makedirs(dss_analysis)
-with open(os.path.join(dss_analysis,'all_voltages.json'),'w') as fp:
-    json.dump(voltages_dict,fp,indent=4, sort_keys=True)
-with open(os.path.join(dss_analysis,'overvoltages.json'),'w') as fp:
-    json.dump(overvoltages_dict,fp,indent=4, sort_keys=True)
-with open(os.path.join(dss_analysis,'undervoltages.json'),'w') as fp:
-    json.dump(undervoltages_dict,fp,indent=4, sort_keys=True)
-with open(os.path.join(dss_analysis,'line_overloads.json'),'w') as fp:
-    json.dump(line_overloads_dict,fp,indent=4, sort_keys=True)
-with open(os.path.join(dss_analysis,'transformer_overloads.json'),'w') as fp:
-    json.dump(transformer_overloads_dict,fp,indent=4, sort_keys=True)
+    for element in voltages:
+        if element not in building_map:
+            continue
+        building_id = building_map[element]
+        if not building_id in voltage_df_dic:
+            voltage_df_dic[building_id] = pd.DataFrame(columns=['Datetime','p.u. voltage','overvoltage','undervoltage'])
+        voltage_df_dic[building_id].loc[i] = [time,voltages[element],voltages[element] >1.05, voltages[element]<0.95]
+    for element in line_overloads:
+        if not element in line_df_dic:
+            line_df_dic[element] = pd.DataFrame(columns=['Datetime','p.u. loading','overloaded'])
+        line_df_dic[element].loc[i] = [time,line_overloads[element],line_overloads[element] >1.0]
+    for element in overloaded_xfmrs:
+        if not element in transformer_df_dic:
+            transformer_df_dic[element] = pd.DataFrame(columns = ['Datetime','p.u. loading', 'overloaded'])
+        transformer_df_dic[element].loc[i] = [time,overloaded_xfmrs[element], overloaded_xfmrs[element] >1.0]
+
+for element in voltage_df_dic:
+    voltage_df_dic[element].to_csv(os.path.join(dss_analysis,'results','Features',element+'.csv'),header=True,index=False)
+for element in line_df_dic:
+    line_df_dic[element].to_csv(os.path.join(dss_analysis,'results','Lines',element+'.csv'),header=True,index=False)
+for element in transformer_df_dic:
+    transformer_df_dic[element].to_csv(os.path.join(dss_analysis,'results','Transformers',element+'.csv'),header=True,index=False)
