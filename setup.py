d6f592e3dd9136e9a1c16b8bd7a6f702730dc85c
telgindy <tarek.elgindy@nrel.gov>
2020-03-31 15:05:42 -0600

    pulling specific ditto branch to be used for this release

--- setup.py
+++ setup.py
@@ -1,13 +1,17 @@
 from setuptools import setup, find_packages
 from setuptools.command.develop import develop
+import os
+
+command = 'git clone -b timeseries_updates https://github.com/NREL/ditto.git'
+os.system(command)
 
 setup(
     version="0.1.0",
     author="Tarek Elgindy",
     author_email="tarek.elgindy@nrel.gov",
-    packages=find_packages()
-    python_requires '>=3.7'
-    install_requiers=[
+    packages=find_packages(),
+    python_requires='>=3.7',
+    install_requires=[
         'opendssdirect.py'
     ]
 )
